34dc927d4bdaf1a58aa145df2369cf6fa290b7a4
Nguyễn Quốc Khánh <khanh9102004@gmail.com>
2025-03-24 21:46:29 +0700

    hot fix

--- src/main/java/com/TLU/SoundVerse/SoundVerseApplication.java
+++ src/main/java/com/TLU/SoundVerse/SoundVerseApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SoundVerseApplication {
-	static {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoundVerseApplication.class, args);

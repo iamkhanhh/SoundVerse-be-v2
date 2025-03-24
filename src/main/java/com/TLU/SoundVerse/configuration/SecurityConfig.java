8c1e8607ac24ac45b49dba62802df31ec87e6749
Nguyễn Quốc Khánh <khanh9102004@gmail.com>
2025-03-24 20:35:33 +0700

    update security

--- src/main/java/com/TLU/SoundVerse/configuration/SecurityConfig.java
+++ src/main/java/com/TLU/SoundVerse/configuration/SecurityConfig.java
@@ -69,9 +69,9 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addAllowedMethod("*");
-        corsConfiguration.addAllowedHeader("*");
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Access-Control-Allow-Credentials"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
